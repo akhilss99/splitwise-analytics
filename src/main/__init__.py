be70810de0c8dae6d47aae84dcda8f12becf8b90
Akhil S S <akhilss61@gmail.com>
2023-07-15 14:06:56 +0530

    Cloud migration.

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from src.main.com.splitwise.util.ConfigLoad import ConfigUtil
+from google.cloud import logging
 
 CONSUMER_KEY = os.environ['SPLITWISE.CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['SPLITWISE.CONSUMER_SECRET']
@@ -8,3 +9,7 @@ API_KEY = os.environ['SPLITWISE.API_KEY']
 
 CONFIGS = ConfigUtil.load_json("config.json")
 QUERIES = ConfigUtil.get_queries("queries.yaml")
+
+logging_client = logging.Client()
+
+LOGGER = logging_client.logger("ETL-Logger")
--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,9 @@
 import duckdb
+import functions_framework
 from splitwise import Splitwise
 
 from com.splitwise.pipeline.AnalyticsPipeline import AnalyticsPipeline
-from src.main import CONSUMER_KEY, CONSUMER_SECRET, API_KEY, CONFIGS, QUERIES
+from src.main import CONSUMER_KEY, CONSUMER_SECRET, API_KEY, CONFIGS, QUERIES, LOGGER
 from src.main.com.splitwise.ingestion.SplitwiseExtraction import SplitwiseExtractUtil
 from src.main.com.splitwise.warehouse.BigQueryClient import BigQueryLoader
 
@@ -10,16 +11,20 @@ SPLITWISE_CONFIG = CONFIGS.get("splitwise", {})
 BIGQUERY_CONFIG = CONFIGS.get("bigquery", {})
 
 if __name__ == '__main__':
-    splitwise = Splitwise(consumer_key=CONSUMER_KEY,
-                          consumer_secret=CONSUMER_SECRET,
-                          api_key=API_KEY)
+    @functions_framework.http
+    def main(request):
+        splitwise = Splitwise(consumer_key=CONSUMER_KEY,
+                              consumer_secret=CONSUMER_SECRET,
+                              api_key=API_KEY)
+        ingestion_util = SplitwiseExtractUtil(splitwise, SPLITWISE_CONFIG)
 
-    ingestion_util = SplitwiseExtractUtil(splitwise, SPLITWISE_CONFIG)
-    loader = BigQueryLoader(BIGQUERY_CONFIG)
+        loader = BigQueryLoader(BIGQUERY_CONFIG)
 
-    conn = duckdb.connect()
+        conn = duckdb.connect()
 
-    pipeline = AnalyticsPipeline(db_connection=conn,
-                                 extraction_util=ingestion_util,
-                                 bq_loader=loader)
-    pipeline.execute(query_dict=QUERIES)
+        pipeline = AnalyticsPipeline(db_connection=conn,
+                                     extraction_util=ingestion_util,
+                                     bq_loader=loader)
+        result = pipeline.execute(query_dict=QUERIES)
+        LOGGER.info(result)
+        return result
