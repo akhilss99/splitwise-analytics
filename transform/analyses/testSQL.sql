dfb3d42be8e5dd0582eaf07a00994742703df3f4
Akhil S S <akhilss61@gmail.com>
2023-08-16 00:29:36 +0000

    Added test cases, documentations

--- transform/analyses/testSQL.sql
+++
@@ -1,9 +0,0 @@
--- analyses/my_analysis.sql
-
--- Import the macro from the dbt_utils package
-
--- Use the macro
-SELECT
-    {{ generate_purchase_types('description', var('purchase_types')) }} AS status_label
-FROM
-    my_table
